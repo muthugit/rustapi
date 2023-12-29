1680105f6442b58c0b8080a18b2dd09061ad3b56
Muthupandian <muthupandianthangarajan@gmail.com>
2023-12-30 02:41:03 +0530

    implemented class

--- src/controllers/post.rs
+++ src/controllers/post.rs
@@ -5,12 +5,16 @@ use post::PostUsecase;
 
 // A function to build our routes
 pub fn routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    get_post()
+    let post_usecase = PostUsecase::new(String::from("Hello, Warp!"));
+    get_post(post_usecase)
 }
 
-// A route to handle GET requests for a specific post
-fn get_post() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+fn get_post(post_usecase: PostUsecase) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    let post_usecase = warp::any().map(move || post_usecase.clone());
     warp::path!("posts" / u64)
         .and(warp::get())
-        .and_then(PostUsecase::get_post)
+        .and(post_usecase)
+        .and_then(|id: u64, post_usecase: PostUsecase| async move {
+            post_usecase.get_post(id).await
+        })
 }
--- src/usecases/post.rs
+++ src/usecases/post.rs
@@ -3,16 +3,21 @@
 mod post;
 use post::Post;
 
+#[derive(Clone)]
 pub struct PostUsecase {
+    pub title: String,
 }
 
 impl PostUsecase {
+    pub fn new(title: String) -> Self {
+        PostUsecase { title }
+    }
     // A function to handle GET requests at /posts/{id}
-    pub async fn get_post(id: u64) -> Result<impl warp::Reply, warp::Rejection> {
+    pub async fn get_post(&self, id: u64) -> Result<impl warp::Reply, warp::Rejection> {
         // For simplicity, let's say we are returning a static post
         let post: Post = Post {
-            id,
-            title: String::from("Hello, Warp!"),
+            id: id,
+            title: self.title.clone(),
             body: String::from("This is a post about Warp."),
         };
         Ok(warp::reply::json(&post))
